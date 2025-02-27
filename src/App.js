e08d26d292282766bf3f08cf2a423812f228aa32
siyoung112 <siyoung112@naver.com>
2025-02-27 04:14:26 +0000

    Add new blog verison 2

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog Version 1
+            EKS DEMO Blog Version 2
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
